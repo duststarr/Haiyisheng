22fc69c578366f5c47ff071d1142a8dae92b0984
duststarr <duststarr@hotmail.com>
2020-05-10 12:34:04 +0800

    basic page

---
+++ miniprogram/pages/product/list.js
@@ -0,0 +1,25 @@
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  data: {
+    cardCur: 0,
+    swiperList: [
+      {
+        id: 0,
+        type: 'image',
+        url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+      },
+      {
+        id: 0,
+        type: 'image',
+        url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg'
+      },
+      {
+        id: 0,
+        type: 'image',
+        url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+      }
+    ]
+  }
+})
