c6c1f96abaa268e08aeeadc6596355d0420332b4
duststarr <duststarr@hotmail.com>
2020-05-10 11:28:22 +0800

    test pass cloud

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -45,6 +45,25 @@ Page({
       })
     }
   },
+  
+  onGetOpenid: function() {
+    console.log('onGetOpenid')
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../about/about',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
+  },
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
