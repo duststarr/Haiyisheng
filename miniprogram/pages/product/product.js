6d9e4143914d2d561feb4246454159968c7c8ff8
duststarr <duststarr@hotmail.com>
2020-05-16 22:25:02 +0800

    product and order page

---
+++ miniprogram/pages/product/product.js
@@ -0,0 +1,31 @@
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  data: {
+    cardCur: 0,
+    swiperList: [
+      {
+        id: 2,
+        type: 'image',
+        url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+      }
+    ]
+  },
+  attached: function(){
+    const db = wx.cloud.database();
+    db.collection('swiper').get().then( res => {
+      this.setData({
+        swiperList: res.data
+      })
+    })
+  },
+  methods: {
+    showIntro: function(e){
+      const id = e.currentTarget.dataset.id
+      wx.navigateTo({
+        url: '/pages/order/order?id='+id
+      })
+    }
+  }
+})
