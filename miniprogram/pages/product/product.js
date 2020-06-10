bc1e575b2b0b14c7798d2e4a0e57cd50e47117a1
duststarr <duststarr@hotmail.com>
2020-06-10 10:15:30 +0800

    feat(product): 优化首屏；增加图片预览

--- miniprogram/pages/product/product.js
+++ miniprogram/pages/product/product.js
@@ -8,23 +8,29 @@ Component({
       {
         id: 0,
         type: 'image',
-        url: 'cloud://hys-k9bhf.6879-hys-k9bhf-1302238335/1.png'
+        url: '/images/1.jpg',
       },
       {
         id: 1,
         type: 'image',
-        url: 'cloud://hys-k9bhf.6879-hys-k9bhf-1302238335/2.png'
+        url: 'cloud://hys-k9bhf.6879-hys-k9bhf-1302238335/2.png',
       },
       {
         id: 2,
         type: 'image',
-        url: 'cloud://hys-k9bhf.6879-hys-k9bhf-1302238335/3.png'
+        url: 'cloud://hys-k9bhf.6879-hys-k9bhf-1302238335/3.png',
       },
       {
         id: 3,
         type: 'image',
-        url: 'cloud://hys-k9bhf.6879-hys-k9bhf-1302238335/4.png'
+        url: 'cloud://hys-k9bhf.6879-hys-k9bhf-1302238335/4.png',
       }
+    ],
+    imgs:[
+      "cloud://hys-k9bhf.6879-hys-k9bhf-1302238335/1.png",
+      "cloud://hys-k9bhf.6879-hys-k9bhf-1302238335/2.png",
+      "cloud://hys-k9bhf.6879-hys-k9bhf-1302238335/3.png",
+      "cloud://hys-k9bhf.6879-hys-k9bhf-1302238335/4.png",
     ]
   },
   attached: function () {
@@ -35,6 +41,14 @@ Component({
       const id = e.currentTarget.dataset.id
       wx.navigateTo({
         url: '/pages/order/order?id=' + id
+      })
+    },
+    onClickImg: function(e){
+      const pos = e.currentTarget.dataset.pos;
+      const that = this;
+      wx.previewImage({
+        current: that.data.imgs[pos],
+        urls: that.data.imgs,
       })
     }
   }
