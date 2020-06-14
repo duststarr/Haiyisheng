2f8a2774e8ab40e501cb5fd4c098dbd19e340f39
duststarr <duststarr@hotmail.com>
2020-06-14 16:25:40 +0800

    important: 低版本兼容。主要是cloud.watch的替代方案

--- cloudfunctions/pay/index.js
+++ cloudfunctions/pay/index.js
@@ -5,21 +5,25 @@ cloud.init({
 })
 const Mch_id = '1599808241'
 exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext() // 必须在main函数里执行，操
+
+  console.log(event)
+  console.log(wxContext.OPENID)
   const bodyMsg = event.bodyMsg;
   const totalFee = event.totalFee;
   const outTradeNo = event.outTradeNo;
   const nonceStr = event.nonceStr;
 
   const res = await cloud.cloudPay.unifiedOrder({
-    "body" : bodyMsg,
-    "outTradeNo" : outTradeNo,
-    "subMchId" : Mch_id,
-    "totalFee" : totalFee,
+    "body": bodyMsg,
+    "outTradeNo": outTradeNo,
+    "subMchId": Mch_id,
+    "totalFee": totalFee,
     "nonceStr": nonceStr,
     "envId": "hys",
     "functionName": "pay_cb",
-    "tradeType":"JSAPI",
-    "spbillCreateIp" : "127.0.0.1"
+    "tradeType": "JSAPI",
+    "spbillCreateIp": "127.0.0.1"
   })
   return res
 }
--- miniprogram/pages/payment/payment.js
+++ miniprogram/pages/payment/payment.js
@@ -7,12 +7,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-    policies: [
-      { content: '充值一年365天，730元（平均2元/天）', days: 365, amount: 730 },
-      { content: '充值两年730天，1388元（平均1.9元/天）', days: 730, amount: 1388 },
-      { content: '充值三年1095天，1971元（平均1.8元/天）', days: 1095, amount: 1971 },
-      { content: '充值四年1460天，2482元（平均1.7元/天）', days: 1460, amount: 2482 },
-      { content: "充值五年1825天，2737元（平均1.5元/天）\n机器所有权、产权归客户", days: 1825, amount: 2737 },
+    policies: [{
+        content: '充值一年365天，730元（平均2元/天）',
+        days: 365,
+        amount: 730
+      },
+      {
+        content: '充值两年730天，1388元（平均1.9元/天）',
+        days: 730,
+        amount: 1388
+      },
+      {
+        content: '充值三年1095天，1971元（平均1.8元/天）',
+        days: 1095,
+        amount: 1971
+      },
+      {
+        content: '充值四年1460天，2482元（平均1.7元/天）',
+        days: 1460,
+        amount: 2482
+      },
+      {
+        content: "充值五年1825天，2737元（平均1.5元/天）\n机器所有权、产权归客户",
+        days: 1825,
+        amount: 2737
+      },
     ],
     orderID: null
   },
@@ -40,7 +59,11 @@ Page({
         icon: 'none',
         duration: 2000,
         success: () => {
-          setTimeout(wx.navigateBack, 2000)
+          setTimeout(() => {
+            wx.redirectTo({
+              url: '/pages/home/home',
+            })
+          }, 2000)
         }
       })
     } catch (e) {
